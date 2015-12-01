c809abaa82e15d4683fec960ec90de8830749d9f
Reggie S <reggie@bgcreatives.com>
2015-11-30 23:42:01 -0500

    test4

--- index.js
+++ index.js
@@ -1,40 +1,32 @@
 ﻿'use strict';
-console.log(process.env.PORT);
-console.log("starting app");
 
 var express = require('express'),
-/*        routes = require('./app/routes/index.js'),
-  mongoose = require('mongoose'), 
-    passport  = require('passport'),
-      session = require('express-session');
-*/         app = express();
+            app = express();
 
 app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(process.cwd() + '/public'));
-
 app.route('/')
   .get(function(req, res) {
-     res.send("GET request to homepage");
+     res.send("hello");
   })
-  
+
 app.listen(app.get('port'), function() {
    console.log("Listening on port: " + app.get('port') + "...");
 });
+/* var express = require('express'),
+        routes = require('./app/routes/index.js'),
+  mongoose = require('mongoose'), 
+    passport  = require('passport'),
+      session = require('express-session');
 
-/*
-if(!process.env.APP_URL) {
-  console.log("loading env file...");
-  require('dotenv').load();
-}
-
-require('./app/config/passport')(passport); 
+var app = express();
+require('dotenv').load();
+require('./app/config/passport')(passport);
 
 mongoose.connect(process.env.MONGO_URI); 
     
-app.use('/controllers', express.static(__dirname + '/app/controllers'));    
-app.use('/public', express.static(__dirname  + '/public'));
-app.use('/common', express.static(__dirname + '/app/common'));
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));    
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/common', express.static(process.cwd() + '/app/common'));
 
 app.use(session({
    secret: 'secretClementine', 
@@ -50,10 +42,10 @@ routes(app, passport);
 var port = process.env.PORT || 8090;
 app.listen(port, function() {
     console.log('Listening on port '+ port + '...');
-});
+}); */
 
 
-*/
+
 
 
 
